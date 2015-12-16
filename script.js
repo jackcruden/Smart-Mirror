399fe0610847295dafdc5dbb5e1ac9df2a49677f
Jack Cruden <jack@jackcruden.com>
2015-12-16 13:01:00 +1300

    Finished time and current weather

---
+++ script.js
@@ -0,0 +1,91 @@
+function time() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+
+    if (m < 10) {
+        m = "0" + m
+    };
+
+    document.getElementById('time_time').innerHTML = h + ":" + m;
+
+    //setInterval(time, 1000); // Every second
+}
+
+function date() {
+    var days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
+
+    var months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+    var today = new Date();
+    var day = today.getDay();
+    var date = today.getDate();
+    var month = today.getMonth();
+
+    $('#time_date').html(days[day] + ", " + date + " " + months[month]);
+
+    //setInterval(date, 1000); // Every second
+}
+
+function weather() {
+    var codes = {
+        '01d': 'sun',           '01n': 'moon',
+        '02d': 'sun_cloud',     '02n': 'cloud',
+        '03d': 'cloud',         '03n': 'cloud',
+        '04d': 'cloud',         '04n': 'cloud',
+        '09d': 'rain_light',    '09n': 'rain_light',
+        '10d': 'sun_rain',      '10n': 'rain',
+        '11d': 'thunder',       '11n': 'thunder',
+        '13d': 'snow',          '13n': 'snow'
+    };
+
+    // Get the new weather info
+    $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=Okato,NZ&units=metric&appid=0f171838ffe7d226be21b8e88ebc6956", function(data) {
+        //console.log(data);
+
+        // Assign data to variables
+        var weatherIcon = '/images/' + codes[data.list[0].weather[0].icon] + '.png';
+        var tempHigh = Math.round(data.list[0].main.temp_max);
+        var tempLow = Math.round(data.list[0].main.temp_min);
+        var weatherDescription = data.list[0].weather[0].description;
+
+        console.log(weatherIcon);
+
+        $('#weather_now_icon').attr('src', weatherIcon);
+        $('#weather_now_temp_high').html(tempHigh);
+        $('#weather_now_temp_low').html(tempLow);
+        $('#weather_now_description').html(weatherDescription);
+    });
+
+    // Update the display
+
+
+    //setInterval(weather, 1800000); // Every half hour
+}
+
+$(document).ready(function() {
+    time();
+    date();
+    weather();
+});
